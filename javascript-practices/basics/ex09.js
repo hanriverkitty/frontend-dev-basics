c00fcb0abab6787569827d8fec38fae5df7f3196
hanriverkitty <danieltls@naver.com>
2024-07-03 15:40:56 +0900

    js basic #5

--- javascript-practices/basics/ex09.js
+++ javascript-practices/basics/ex09.js
@@ -10,5 +10,20 @@ var MyObject = function (name, age) {
 MyObject.prototype.school = "bitacademy";
 MyObject.prototype.course = "poscodx 8기";
 MyObject.prototype.info = function () {
-  console.log(this.school + ":" + this.course);
+  console.log(
+    this.name + ":" + this.age + ":" + this.school + ":" + this.course
+  );
 };
+
+// MyObject 인스턴스 생성1
+var o1 = new MyObject("둘리", 10);
+o1.info();
+o1.school = "poscodx";
+o1.info();
+
+// MyObject 인스턴스 생성2
+var o2 = new MyObject("마이콜", 20);
+o2.info = function () {
+  console.log("무얏호우~~");
+};
+o2.info();
