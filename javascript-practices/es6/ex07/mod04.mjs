e885cb64bc471c75a5a285da60170804897adc91
hanriverkitty <danieltls@naver.com>
2024-07-09 16:55:10 +0900

    es6 #4

---
+++ javascript-practices/es6/ex07/mod04.mjs
@@ -0,0 +1,17 @@
+/**
+ *
+ * named export + unamed export
+ *
+ *
+ */
+
+const add = function (a, b) {
+  return a + b;
+};
+
+const subtract = function (a, b) {
+  return a - b;
+};
+
+export { add, subtract };
+export default { add, subtract };
